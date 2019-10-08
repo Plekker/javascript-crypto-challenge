e79b020db33e3fc33cc6741a1f8523e637d1540c
bram.de.plekker@student.ehb.be <bram.de.plekker@student.ehb.be>
2019-10-08 20:35:37 +0200

    Update Signature.js
    Sign tests

--- src/Signature.js
+++ src/Signature.js
@@ -1,2 +1,22 @@
-const sodium = require('libsodium-wrappers');
+const nacl = require('libsodium-wrappers');
+
+let keypair = null;
+
+(async () => {
+    await nacl.ready;
+    keypair = nacl.crypto_sign_keypair();
+})();
+
+async function verifyingKey(){
+    await nacl.ready; 
+    return keypair.publicKey;
+}
+
+async function sign(msg){
+    await nacl.ready;
+    return nacl.crypto_sign(msg, keypair.privateKey);
+}
+
+module.exports.verifyingKey = verifyingKey;
+module.exports.sign = sign;
 
